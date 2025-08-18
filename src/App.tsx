d7114b08fdca95dfa2a499f4d498ed39739907b6
HowardGA <131924292+HowardGA@users.noreply.github.com>
2025-08-18 13:41:26 -0700

    Lab 01

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Header from './components/Header';
+function App() {
+
+  return (
+    <>
+      <Header />
+      <p>Este es el Lab 01</p>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Mi primer componente</h1>
+    </header>
+  );
+};
+
+export default Header;
